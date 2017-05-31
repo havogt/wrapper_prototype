5aa6410f70f7d27fef8db502f97957f87f75ad58
Hannes Vogt <hannes@havogt.de>
2017-05-31 19:14:46 +0200

    TODOs

--- usercode/dycore_example/main.c
+++ usercode/dycore_example/main.c
@@ -5,6 +5,14 @@
 #include "../../wrapper/include/wrapper_functions.h"
 #include "dycore_init.h"
 
+/* TODO
+ * - flag on each field which is modified by the input()/output() parameters and the push/pull automatically
+ * - check in the dycore that all input fields are clean at the beginning, invalidate all inputs at the end()
+ * - invalidate all output fields in the dycore
+ * - the dycore provides a function to fortran to check if all clean after pull
+ * - usercode returns a layout+raw_ptr to the wrapper to allow the wrapper to do its copies.
+ */
+
 int main() {
     printf("main()\n");
     float *tmp = (float *)malloc(sizeof(float));
